1c927310016209c3bbb77d71e8d890897a48d7e8
charlieangriawan <charlieangriawan@hotmail.com>
2021-01-30 21:37:46 +0800

    Added express server and connection to mongo

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  rootDir: '.', // This should point to the rootDir set in your tsconfig.json
+  globals: {
+    'ts-jest': {
+      tsconfig: './tsconfig.json',
+    },
+  },
+  verbose: true,
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  setupFiles: [
+    'dotenv/config',
+  ],
+  modulePathIgnorePatterns: [
+    '<rootDir>/dist/',
+  ],
+  moduleNameMapper: {
+    '@mongo/(.*)': '<rootDir>/src/mongo/$1',
+    '@server/(.*)': '<rootDir>/src/server/$1',
+  },
+};
---
+++ src/server/router.ts
@@ -0,0 +1,83 @@
+import 'express-async-errors';
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import mongoSanitize from 'express-mongo-sanitize';
+
+class Routes {
+  private baseRouteDocs: any;
+
+  private baseRoute: any;
+
+  private router: any;
+
+  constructor() {
+    this.baseRoute = '/api/v1';
+    this.router = express.Router();
+
+    this.initializeParser();
+    this.initializeRootRoute();
+    this.initializeErrorHandling();
+  }
+
+  getRouter(): any {
+    return this.router;
+  }
+
+  static configCors(whitelist: string[]) {
+    let allowAll = false;
+    if (whitelist.length === 0) allowAll = true;
+    return {
+      methods: 'POST,PUT,PATCH,GET,UPDATE,DELETE',
+      allowedHeaders: [
+        'Origin',
+        'X-Requested-With',
+        'Content-Type',
+        'Authorization',
+      ],
+      credentials: true,
+      origin: (origin: string, callback: any) => {
+        if (allowAll || whitelist.indexOf(origin) !== -1) {
+          callback(null, true);
+        } else {
+          callback({
+            name: 'UnauthorizedOriginError',
+            message: `Unauthorized 401: CORS from Origin ${origin} not Allowed`,
+          });
+        }
+      },
+    };
+  }
+
+  private initializeParser() {
+    this.router.use(cookieParser());
+    this.router.use(bodyParser.json());
+    this.router.use(bodyParser.urlencoded({ extended: false }));
+    this.router.use(mongoSanitize({ replaceWith: '_' }));
+  }
+
+  private initializeRootRoute() {
+    this.router.get('/', (req: any, res: any) => {
+      res.status(200).json({ message: true });
+    });
+
+    this.router.get(`${this.baseRoute}/`, (req: any, res: any) => {
+      res.status(200).json({ message: true });
+    });
+  }
+
+  private initializeErrorHandling() {
+    this.router.use((err: any, req: any, res: any, next: any) => {
+      if (err.name === 'UnauthorizedError') {
+        res.status(401).json({ error: true });
+      } else if (err.name === 'UnauthorizedOriginError') {
+        res.status(401).json({ error: true });
+      } else {
+        next(err);
+      }
+    });
+  }
+}
+
+export default Routes;
